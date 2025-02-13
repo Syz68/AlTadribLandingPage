cae367a4afdeb8469b590155a581778295e5a364
Vicky Kumar <89451580+Syz68@users.noreply.github.com>
2025-02-13 12:58:18 +0530

    button change
    move the button to bottom in mobile view and fixed responsiveness

--- src/altadrib/components/FormSection.jsx
+++ src/altadrib/components/FormSection.jsx
@@ -1,15 +1,23 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './FormSection.css';
 
 const FormSection = () => {
+    const [isLoading, setIsLoading] = useState(true);
+
+    const handleIframeLoad = () => {
+        setIsLoading(false);
+    };
+
     return (
         <div className='formcontainer'>
             <h1>Registration Form</h1>
+            {isLoading && <div className="spinner">Loading...</div>}
             <iframe
                 src="https://asterclinicmern.stldigitaltech.com/form/677ce536a9c75b6eb5bd5175?redirect_to=https://asterclinicmern.stldigitaltech.com/"
                 frameBorder="0"
                 className='iframecontainer'
                 scrolling='no'
+                onLoad={handleIframeLoad}
             ></iframe>
         </div>
     );
